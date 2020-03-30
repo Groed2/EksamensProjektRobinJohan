c50dd0787e00cbd15af3d84614fafbd4b17a8b43
Groed <Johan.hammer.jakobsen@gmail.com>
2020-03-30 12:53:38 +0200

    DAO Oprettet
    DAO oprettet, funktioner og Queries lavet
    
    Selection, Deletion, Update og Insertion queries er lavet, funktionerne hertil er ikke helt forstået.

--- app/src/main/java/Day.java
+++ app/src/main/java/Day.java
@@ -1,3 +1,4 @@
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
@@ -13,4 +14,22 @@ public class Day {
     private Date mdate;
     @ColumnInfo (name = "BMI")
     private float mBMI;
+
+    Day(float BMI, Date date)
+    {
+        this.mBMI = BMI;
+        this.mdate = date;
+    }
+
+    public Date getMdate() {
+        return mdate;
+    }
+
+    public float getmBMI() {
+        return mBMI;
+    }
+
+    public int getDayId() {
+        return dayId;
+    }
 }
